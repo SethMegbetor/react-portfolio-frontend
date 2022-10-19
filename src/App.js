552cc2f50654d06ff1b17b9abf5230b2085db26f
“SethMegbetor” <sethmegbetor@gmail.com>
2022-10-19 19:05:18 +0000

    header section done and working

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <About />
       <Header />
+      <About />
       <Work />
       <Skills />
       <Testimonial />
