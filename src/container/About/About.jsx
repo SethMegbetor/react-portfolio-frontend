16b8a1845e7be0a57b49a118400f74996acc6039
“SethMegbetor” <sethmegbetor@gmail.com>
2022-10-20 09:27:36 +0000

    can fetch data from sanity for the about section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,38 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-import { images } from "../../constants";
+// import { images } from "../../constants";
 import "./About.scss";
+import { urlFor, client } from "../../client";
 
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am the world's best",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Design",
-    description: "I am the world's best",
-    imgUrl: images.about02,
-  },
-  {
-    title: "UI/UX",
-    description: "I am the world's best",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Web Animations",
-    description: "I am the world's best",
-    imgUrl: images.about04,
-  },
-];
+// const abouts = [
+//   {
+//     title: "Web Development",
+//     description: "I am the world's best",
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: "Web Design",
+//     description: "I am the world's best",
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: "UI/UX",
+//     description: "I am the world's best",
+//     imgUrl: images.about03,
+//   },
+//   {
+//     title: "Web Animations",
+//     description: "I am the world's best",
+//     imgUrl: images.about04,
+//   },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
-        I know that a
-        <span> Great Developer </span>
+        I know that a<span> Great Developer </span>
         <br />
         means
         <span> Good Business </span>
@@ -47,7 +55,8 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            {/* <img src={about.imgUrl} alt={about.title} /> */}
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
